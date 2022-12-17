c0e9907d5c7e01b2a12500622c3a7ab427ae2db8
GSP <Gowri.Shankar@tecnotree.com>
2022-12-17 12:58:29 +0530

    initial changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
-import './App.css';
-
+import "./App.css";
+import Footer from "./components/Footer";
+import Home from "./Home";
 function App() {
   return (
     <div className="app">
-      <h1> welcome</h1>
+      <Home />
+      <Footer />
     </div>
   );
 }
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Header from './components/Header'
 import './Home.css'
 
 function Home() {
   return (
     <div>
+    <Header />
       <div className="home">
         <Link to='/'>
           <h1>Home </h1>
         </Link>
-        <Link to='/signUp'>
+        {/* <Link to='/signUp'>
           <h2> SignUp </h2>
-        </Link>
+        </Link> */}
         <Link to='/signIn'>
           <h2> SignIn </h2>
         </Link>
