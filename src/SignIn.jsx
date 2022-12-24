3452aafdf6e3eec6a50c23ad661f1739bf3165b2
Pavithran2 <pavithrank02@gmail.com>
2022-12-24 18:45:04 +0530

    SignIn UI completed

---
+++ src/SignIn.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { useState } from "react";
+import { IconButton } from "@mui/material";
+import Button from '@mui/material/Button';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TextField from '@mui/material/TextField';
+import GoogleIcon from '@mui/icons-material/Google';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import "./SignIn.css";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';import Home from "./Home";
+import { useNavigate } from "react-router-dom";
+
+function SignIn() {
+  
+
+  return (
+    <div>
+      <form>
+        <div className="container">
+          <div className="signin">
+          <h2>Login</h2>
+          <p>Add Your Credentials Below..</p>
+          <IconButton className="icon">
+            <AccountCircleIcon sx={{ fontSize: 70, color: "#1957DD"}}  />
+          </IconButton>
+          <TextField
+            margin="dense"
+            color="primary"
+            id="outlined-multiline-flexible"
+            label="Username"
+            name="username"
+            onChange={inputHanlder}
+            value={data.username}
+          />
+          <TextField
+            margin="normal"
+            id="outlined-multiline-flexible"
+            sx={{color: "white"}}
+            label="Password"
+            name="password" 
+            backgroundColor="white"
+            onChange={inputHanlder} 
+            value={data.password} />
+          {<Button variant="contained" size="medium" onClick={submitButton} type="submit">Login</Button>}
+          <p>------or Signin Through------</p>
+          <div className="log">
+          <GoogleIcon sx={{color:"red"}}/>
+          <FacebookIcon sx={{color:"blue"}}/>
+          <TwitterIcon sx={{color:"#19A2DD"}}/>
+          </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default SignIn;
