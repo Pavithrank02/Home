c6f6d2152648a8a5cd647a795f7a95d0b6825c58
Pavithran2 <pavithrank02@gmail.com>
2022-12-24 18:38:58 +0530

    final UI

--- src/SignIn.js
+++ src/SignIn.js
@@ -7,7 +7,7 @@ import TextField from '@mui/material/TextField';
 import GoogleIcon from '@mui/icons-material/Google';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import "./SignIn.css";
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';import Home from "./Home";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle'; import Home from "./Home";
 import { useNavigate } from "react-router-dom";
 
 function SignIn() {
@@ -39,7 +39,7 @@ function SignIn() {
     // } else {
     //   console.log("doesnt match");
     // }
-    const { username, password } = data;
+    const { username, password } = [data];
     if (username === "") {
       alert("Please enter username");
     } else if (password.length < 5) {
@@ -62,36 +62,35 @@ function SignIn() {
       <form>
         <div className="container">
           <div className="signin">
-          <h2>Login</h2>
-          <p>Add Your Credentials Below..</p>
-          <IconButton className="icon">
-            <AccountCircleIcon sx={{ fontSize: 70, color: "#1957DD"}}  />
-          </IconButton>
-          <TextField
-            margin="dense"
-            color="primary"
-            id="outlined-multiline-flexible"
-            label="Username"
-            name="username"
-            onChange={inputHanlder}
-            value={data.username}
-          />
-          <TextField
-            margin="normal"
-            id="outlined-multiline-flexible"
-            sx={{color: "white"}}
-            label="Password"
-            name="password" 
-            backgroundColor="white"
-            onChange={inputHanlder} 
-            value={data.password} />
-          {<Button variant="contained" size="medium" onClick={submitButton} type="submit">Login</Button>}
-          <p>------or Signin Through------</p>
-          <div className="log">
-          <GoogleIcon sx={{color:"red"}}/>
-          <FacebookIcon sx={{color:"blue"}}/>
-          <TwitterIcon sx={{color:"#19A2DD"}}/>
-          </div>
+            <h2>Login</h2>
+            <p>Add Your Credentials Below..</p>
+            <IconButton className="icon">
+              <AccountCircleIcon sx={{ fontSize: 70, color: "#1957DD" }} />
+            </IconButton>
+            <TextField
+              margin="dense"
+              color="primary"
+              id="outlined-multiline-flexible"
+              label="Username"
+              name="username"
+              onChange={inputHanlder}
+              value={data.username}
+            />
+            <TextField
+              margin="normal"
+              id="outlined-multiline-flexible"
+              sx={{ color: "white" }}
+              label="Password"
+              name="password"
+              onChange={inputHanlder}
+              value={data.password} />
+            {<Button variant="contained" size="medium" onClick={submitButton} type="submit">Login</Button>}
+            <p>------or Signin Through------</p>
+            <div className="log">
+              <GoogleIcon sx={{ color: "red" }} />
+              <FacebookIcon sx={{ color: "blue" }} />
+              <TwitterIcon sx={{ color: "#19A2DD" }} />
+            </div>
           </div>
         </div>
       </form>
