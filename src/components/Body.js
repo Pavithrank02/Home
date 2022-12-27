ec841a588c477f0892e0b0f3d0d0ccd4e0ca0016
Pavithran2 <pavithrank02@gmail.com>
2022-12-27 18:05:00 +0530

    a component added

---
+++ src/components/Body.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Routes, Route, Link } from 'react-router-dom';
+import Grid from '@mui/material/Grid';
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import SignIn from './../SignIn';
+import SignUp from './../SignUp';
+import pic from "./../images/signin.jpg"
+import './../Home.css'
+
+function Body() {
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(3),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
+  return (
+    <Grid container  spacing={12}>
+      <Grid item xs={6} md={6}>
+        <Item>
+          <h1>Welcome to Website</h1>
+          <h4>If new, Click here to <span> <Link to='/signup' >
+            SignUp
+          </Link></span></h4>
+          <img className='image-sign' src={pic} alt='image' />
+        </Item>
+      </Grid>
+      <Grid item xs={6} md={4} justifyContent="center" alignItems="center">
+          <Routes>
+            <Route path="/signIn" element={<SignIn />} />
+          </Routes>
+          <Routes>
+            <Route path="/signUp" element={<SignUp />} />
+          </Routes>
+      </Grid>
+    </Grid>
+  )
+}
+
+export default Body
