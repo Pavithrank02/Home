016df97739e5530b0b8b1502c8d8411b184b867e
Pavithran2 <pavithrank02@gmail.com>
2022-12-30 13:02:44 +0530

    chanes

--- src/SignIn.jsx
+++ src/SignIn.jsx
@@ -1,47 +1,85 @@
-import React from "react";
-import { IconButton } from "@mui/material";
-import Button from '@mui/material/Button';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import TextField from '@mui/material/TextField';
-import GoogleIcon from '@mui/icons-material/Google';
-import TwitterIcon from '@mui/icons-material/Twitter';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import IconButton from "@mui/material/IconButton";
+import { Routes, Route, Link } from "react-router-dom";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import TextField from "@mui/material/TextField";
+import GoogleIcon from "@mui/icons-material/Google";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import Home from "./Home";
 import "./Styles.css";
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';import Home from "./Home";
+import { Grid } from "@mui/material";
+
+function SignIn(props) {
+
+  const [formValChange, setFormValChange] = useState("");
 
 
-function SignIn() {
+  // useEffect(() => {
+  //     const inputVal = JSON.parse(localStorage.getItem('formValues'));
+  //     if (inputVal) {
+  //      setInputVal(inputVal);
+  //     }
+  //   }, []);
+  const formChange = () => {
+    
+    console.log(props)
+   props = {
+    setFormType: "signUp"
+   }
+    
+  }
+  
   return (
     <div className="signin">
-          <h2>Login</h2>
-          <p>Add Your Credentials Below..</p>
-          <IconButton className="icon">
-            <AccountCircleIcon sx={{ fontSize: 70, color: "#1957DD" }} />
-          </IconButton>
-          <form >
-            <TextField
-              margin="dense"
-              color="primary"
-              id="outlined-multiline-flexible"
-              label="Username"
-              name="username"
-              
-            />
-            <TextField
-              margin="normal"
-              id="outlined-multiline-flexible"
-              sx={{ color: "white" }}
-              label="Password"
-              name="password"
-               />
-            {<Button variant="contained" size="medium" type="submit">Login</Button>}
-          </form>
-          <p>------or Signin Through------</p>
-          <div className="log">
-            <GoogleIcon sx={{ color: "red" }} />
-            <FacebookIcon sx={{ color: "blue" }} />
-            <TwitterIcon sx={{ color: "#19A2DD" }} />
-          </div>
-        </div>
+      <h2>Login</h2>
+      <p>Add Your Credentials Below..</p>
+      <IconButton className="icon">
+        <AccountCircleIcon sx={{ fontSize: 70, color: "#1957DD" }} />
+      </IconButton>
+      <form>
+        <TextField
+          margin="dense"
+          color="primary"
+          id="outlined-multiline-flexible"
+          label="Username"
+          name="username"
+          onChange={inputHanlder}
+          value={data.username}
+        />
+        <TextField
+          margin="normal"
+          id="outlined-multiline-flexible"
+          sx={{ color: "white" }}
+          label="Password"
+          name="password"
+          onChange={inputHanlder}
+          value={data.password}
+        />
+        {
+          <Button
+            variant="contained"
+            size="medium"
+            onClick={submitButton}
+            type="submit"
+          >
+            Login
+          </Button>
+        }
+        <Typography variant="p">
+          If you are new, <Button onClick={formChange}>SignUp </Button>
+        </Typography>
+      </form>
+      <p>------or Signin Through------</p>
+      <div className="log">
+        <GoogleIcon sx={{ color: "red" }} />
+        <FacebookIcon sx={{ color: "blue" }} />
+        <TwitterIcon sx={{ color: "#19A2DD" }} />
+      </div>
+    </div>
   );
 }
 
