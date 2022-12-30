6e65bbf500019e6809ea14296dd0c3ec24392856
Pavithran2 <pavithrank02@gmail.com>
2022-12-30 12:57:24 +0530

    changes in logic

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,39 +1,27 @@
-import React from 'react'
-import { Routes, Route, Link } from 'react-router-dom';
-import Grid from '@mui/material/Grid';
-import { styled } from '@mui/material/styles';
-import Paper from '@mui/material/Paper';
-import SignIn from './../SignIn';
-import pic from "./../images/signin.jpg"
+import React, {useState} from "react";
+import Grid from "@mui/material/Grid";
+import SignIn from "./../SignIn";
+import SignUp from "./../SignUp";
+import pic from "./../images/signin.jpg";
 
-function Body() {
-  const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(3),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  }));
+const Body = () => {
+  const [formType, setFormType] = useState("signIn");
+
   return (
-    <Grid container spacing={12}>
-      <Grid item xs={6} md={6}>
-        <Item>
+    <Grid container spacing={12} >
+      <Grid item xs={8} display="flex" direction="column" justifyContent="center" alignItems="center">
           <h1>Welcome to Website</h1>
-          <h4>If new, Click here to <span> <Link to='/signup' >
+          <h4>
+            If new, Click here to{" "}
             SignUp
-          </Link></span></h4>
-          <img className='image-sign' src={pic} alt='image' />
-        </Item>
+          </h4>
+          <img className="image-sign" src={pic} alt="image" />
       </Grid>
-      <Grid item xs={6} md={4} mx={16} my={15} justifyContent="center" alignItems="center">
-        <Item>
-          <Routes>
-            <Route path="/signIn" element={<SignIn />} />
-          </Routes>
-        </Item>
+      <Grid item xs={4} display="flex" justifyContent="center" alignItems="flex-start"  >
+      { formType==="signIn" ? <SignIn setFormType= {formType}/> :<SignUp /> }
       </Grid>
     </Grid>
-  )
+  );
 }
 
-export default Body
+export default Body;
