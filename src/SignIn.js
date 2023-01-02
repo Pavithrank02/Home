4a20f92ff0e5eb79628ada1bfe421bff7f662f3e
Pavithran2 <pavithrank02@gmail.com>
2023-01-02 12:57:12 +0530

    completed user authentication logic partially

---
+++ src/SignIn.js
@@ -0,0 +1,146 @@
+import React, { useState } from "react";
+import IconButton from "@mui/material/IconButton";
+import Grid from "@mui/material/Grid";
+import Box from '@mui/material/Box';
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import TextField from "@mui/material/TextField";
+import GoogleIcon from "@mui/icons-material/Google";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import Snackbar from '@mui/material/Snackbar';
+import CloseIcon from '@mui/icons-material/Close';
+import "./Styles.css";
+
+function SignIn(props) {
+  const initialData = {
+    username: "",
+    password: ""
+  }
+  const [data, setData] = useState(initialData);
+  const [open, setOpen] = useState(false);
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
+  const inputHanlder = (event) => {
+    const { name, value } = event.target;
+    setData((prev) => {
+      return { ...prev, [name]: value };
+    });
+  };
+  const action = (
+    <React.Fragment>
+      <Button color="secondary" size="small" onClick={handleClose}>
+        UNDO
+      </Button>
+      <IconButton
+        size="small"
+        aria-label="close"
+        color="inherit"
+        onClick={handleClose}
+      >
+        <CloseIcon fontSize="small" />
+      </IconButton>
+    </React.Fragment>
+  );
+
+  const formChange = () => {
+    const { setFormType } = props;
+    setFormType("signup");
+  };
+  const submitButton = (e) => {
+    e.preventDefault();
+
+    const userDetails = localStorage.getItem("formsValues");
+    const { username, password } = data;
+    if (username === "") {
+      alert("Please enter username");
+    } else if (password.length < 5) {
+      alert("Please enter valid password");
+    } else {
+      if (userDetails && userDetails.length) {
+        const userData = JSON.parse(userDetails);
+        userData.forEach(e => {
+          if (e.username === username && e.password === password) {
+            setOpen(true);
+          } else {
+            alert("not matched");
+          }
+        });
+
+      }
+      setData(initialData);
+    }
+  };
+
+  return (
+    <Box sx={{ border: 2, mt: 3, borderColor: 'primary.main', borderRadius: '2%', height: '90%' }}>
+      <Grid container display="flex" direction="column" justifyContent="center" alignItems="center">
+        <Typography variant="h4" marginTop={2} sx={{ fontWeight: 'Bold' }}>
+          Login
+        </Typography>
+        <Typography variant="p" marginTop={1}>
+          Add Your Credentials Below..
+        </Typography>
+        <IconButton className="icon">
+          <AccountCircleIcon sx={{ fontSize: 70, color: "#1957DD" }} />
+        </IconButton>
+        <form className='form-item'>
+          <TextField
+            margin="none"
+            color="primary"
+            id="outlined-multiline-flexible"
+            label="Username"
+            name="username"
+            onChange={inputHanlder}
+            value={data.username}
+          />
+          <TextField
+            margin="normal"
+            id="outlined-multiline-flexible"
+            label="Password"
+            name="password"
+            onChange={inputHanlder}
+            value={data.password}
+          />
+          <Button
+            variant="contained"
+            size="medium"
+            type="submit"
+            marginTop={4}
+            onClick={submitButton}
+          >
+            Login
+          </Button>
+          <Snackbar
+            open={open}
+            autoHideDuration={6000}
+            onClose={handleClose}
+            message="You Have Successfully Logged In!"
+            action={action}
+            required
+          />
+
+          <Typography variant="p" sx={{ fontWeight: 'medium', textAlign: 'center' }} mt={2}>
+            If you are new, <Button justifyContent="center" alignItems="center" onClick={formChange}>SignUp </Button>
+          </Typography>
+        </form>
+        <p>------or Signin Through------</p>
+        <div className="log">
+          <GoogleIcon sx={{ color: "red" }} />
+          <FacebookIcon sx={{ color: "blue" }} />
+          <TwitterIcon sx={{ color: "#19A2DD" }} />
+        </div>
+      </Grid>
+    </Box>
+
+  );
+}
+
+export default SignIn;
---
+++ src/SignUp.js
@@ -0,0 +1,161 @@
+import React, { useState } from 'react'
+import Grid from "@mui/material/Grid";
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Typography from "@mui/material/Typography";
+import Snackbar from '@mui/material/Snackbar';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { IconButton } from "@mui/material";
+import CloseIcon from '@mui/icons-material/Close';
+import './Styles.css'
+
+
+const initialData = {
+  username: "",
+  email: "",
+  address: "",
+  password: ""
+}
+
+function SignUp(props) {
+
+  const [data, setData] = useState(initialData);
+  const [error, setError] = useState(true);
+  const inputHanlder = event => {
+    const { name, value } = event.target;
+    setData(() => {
+      return {
+        ...data, [name]: value
+      }
+
+    });
+
+  }
+  const [open, setOpen] = useState(false);
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  const storeData = () => {
+    let names = JSON.parse(localStorage.getItem("formsValues")) || []
+    let newArr = [...names, data];
+    localStorage.setItem("formsValues", JSON.stringify(newArr))
+  }
+  const formChange = () => {
+    const { setFormType } = props;
+    setFormType("signIn");
+  };
+  const action = (
+    <React.Fragment>
+      <Button color="secondary" size="small" onClick={handleClose}>
+        UNDO
+      </Button>
+      <IconButton
+        size="small"
+        aria-label="close"
+        color="inherit"
+        onClick={handleClose}
+      >
+        <CloseIcon fontSize="small" />
+      </IconButton>
+    </React.Fragment>
+  );
+
+  const submitForm = (e) => {
+    setError(false);
+    e.preventDefault();
+
+
+    // console.log(props.error);
+    const { username, email, address, password } = data;
+    if (username === "" && !email.includes("@") && address === "" && password.length < 5) {
+      return;
+    } else {
+      setData(initialData);
+      storeData();
+      setOpen(true);
+      setError(true);
+    }
+  }
+
+  return (
+    <Box sx={{ border: 2, mt: 3, borderColor: 'primary.main', borderRadius: '2%', height: '95%', width: '70%' }}>
+      <Grid container display="flex" direction="column" justifyContent="center" alignItems="center">
+        <Typography variant="h4" marginTop={2} sx={{ fontWeight: 'Bold' }}>
+          SignUp
+        </Typography>
+        <IconButton className="icon">
+          <AccountCircleIcon sx={{ fontSize: 70, color: "#1957DD" }} />
+        </IconButton>
+        <form className='form-item' noValidate
+          autoComplete="off">
+          <TextField
+            label="Username"
+            margin="dense"
+            color="primary"
+            name="username"
+            required={true}
+            onChange={inputHanlder}
+            value={data.username}
+            error={error ? "" : !data.username}
+            helperText={!data.username ? "Field should not be empty" : ""}
+          />
+
+          <TextField
+            label="Email"
+            margin="dense"
+            color="primary"
+            name="email"
+            onChange={inputHanlder}
+            value={data.email}
+            required={true}
+            error={error ? "" : !data.email.includes("@" && ".com")}
+            helperText={!data.email.includes("@" && ".com") ? "Field should include @" : ""}
+          />
+          <TextField
+            label="Address"
+            margin="dense"
+            color="primary"
+            name="address"
+            onChange={inputHanlder}
+            value={data.address}
+            required={true}
+            error={error ? "" : !data.address}
+            helperText={!data.address ? "Field should not be empty" : ""}
+          />
+          <TextField
+            label="Password"
+            margin="dense"
+            color="primary"
+            name="password"
+            onChange={inputHanlder}
+            value={data.password}
+            error={error ? "" : !data.password}
+            helperText={data.password.length < 5 ? "Password should be greater than 5" : ""}
+          />
+          <Button variant="contained" size="medium" onClick={submitForm}>Submit</Button>
+          <Snackbar
+            open={open}
+            autoHideDuration={6000}
+            onClose={handleClose}
+            message="User Account Created Successfully!"
+            action={action}
+            required
+          />
+        </form>
+        <Typography variant="p">
+          Already Existing User, <Button onClick={formChange}>SignIn </Button>
+        </Typography>
+      </Grid>
+    </Box>
+
+  )
+}
+
+export default SignUp
